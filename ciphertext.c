cdc8aac00bed61ff8ab3738d7873720883462e4f
Frostin <iamfrostin@gmail.com>
2016-06-24 15:43:39 +0530

    First Lab..

---
+++ ciphertext.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main(){
+	char ptext[50],ctext[50],mess[50];
+	int slen,i,key;
+	printf("Enter the Plain Text: \n");
+	scanf("%s",&ptext);
+	printf("Enter the Key: \n");
+	scanf("%d",&key);
+	slen = strlen(ptext);
+	printf("The Encoded Message Is: \n");
+	for(i=0;i<slen;i++){
+		//printf("%c",ptext[i]);
+		//printf("%d",ptext[i]+3);
+		ctext[i] = ((ptext[i]-97)+key%26)+65;
+		printf("%c",ctext[i]);
+	}
+	printf("\n The Dencoded Message Is: \n");
+	for(i=0;i<slen;i++){
+
+		mess[i] = (ctext[i]-65-key+26)%26+97;
+		printf("%c",mess[i]);
+	}
+}
---
+++ tab.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+//Declare Variables Here
+int Aval=65,tab_val[100][100],i,j,plen,klen,x=0;
+char ptext[50],key[50]; 
+
+//Main Function
+main(){
+	printf("Enter the plain text: \n");
+	scanf("%s",&ptext);
+	printf("Enter the key: \n");
+	scanf("%s",&key);
+	plen = strlen(ptext);
+	klen = strlen(key);
+	vtable();
+	encode_op();
+}
+
+//Create The Virtual Table
+void vtable(){
+	for(i=0;i<=25;i++){
+		for(j=0;j<=25;j++){
+			tab_val[i][j] = (Aval + j);
+			if(tab_val[i][j]>90){
+				tab_val[i][j] = tab_val[i][j] - 26;
+			}
+			printf("%c ",tab_val[i][j]);
+		}
+	Aval += 1;
+	printf("\n");
+	}
+}
+
+void encode_op(){
+	//printf("%s",optext);
+	if(klen < plen){
+		for(i=0;i<plen;i++){
+			if(i > klen){
+				key[klen] = key[x];
+				x += 1;
+			}
+		}
+	} 
+printf("Klen:%d\n",klen);
+printf("%s",key);
+}
+
