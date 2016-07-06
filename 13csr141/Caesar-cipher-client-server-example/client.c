76e843a95d69efa405aaeb59b40c6c6232441dd3
Ramkumar.M <ramkumar225555@gmail.com>
2016-07-06 12:57:38 +0530

    Update client.c

--- 13csr141/Caesar-cipher-client-server-example/client.c
+++ 13csr141/Caesar-cipher-client-server-example/client.c
@@ -6,7 +6,7 @@
 #include<string.h>
 void main()
 { 
-    char a[100],c[100],p[100];
+    char a[100],p[100];
     int k,j,i;
   int sid,lid,seid,rid,cid,aid,z;
   char buff[100],msg[100],input[100],c[100];
@@ -15,13 +15,20 @@ void main()
    x.sin_port=htons(5001);
    x.sin_addr.s_addr=inet_addr("127.0.0.1");
    sid=socket(AF_INET,SOCK_STREAM,0);
-
-   printf("enter the message\n");
-   scanf("%s",msg);
+      printf("enter the plain text");
+   scanf("%s",&a);
+      printf("enter the key");
+   scanf("%d",&k);
+	for(i=0;a[i]!='\0';i++)
+	{
+		c[i]=((a[i]-97+k)%26)+65;
+	}
+   
+   
    if(sid==-1)
    printf(" socket error\n");
     cid=connect(sid,(struct sockaddr *) &x,sizeof(x));
-     seid=send(sid,msg,100,0);
-     printf("message send:%s\n",msg);
+     seid=send(sid,c,100,0);
+     printf("message send:%s\n",c);
 
   }
